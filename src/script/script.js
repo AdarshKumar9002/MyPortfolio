49286517c9afda2d2c8aedf7b0cc68567a3e26ae
AdarshKumar9002 <adarashkumar9002@gmail.com>
2024-01-10 20:08:30 +0530

    fix the navbar, heroImage, and footer

--- src/script/script.js
+++ src/script/script.js
@@ -1,2 +1,21 @@
-// dfsdf
+const HAMBURGER_MENU_ELEMENT =document.getElementById("btn-menu");
+const IMG_SRC_ELEMENT =document.getElementById("imgSrc");
+const NAV_CONTAINER_ELEMENT = document.querySelector(".nav__container");
+const body = document.querySelector("body");
 
+const hamburgerMenu = () => {
+    const visibility = NAV_CONTAINER_ELEMENT.getAttribute("data-visible");
+    if (visibility === "false") {
+      NAV_CONTAINER_ELEMENT.setAttribute("data-visible", true);
+      HAMBURGER_MENU_ELEMENT.setAttribute("aria-expanded", true);
+      IMG_SRC_ELEMENT.src = "img/x.svg";
+      body.style.overflow = "hidden"; 
+    } else {
+      NAV_CONTAINER_ELEMENT.setAttribute("data-visible", false);
+      HAMBURGER_MENU_ELEMENT.setAttribute("aria-expanded", false);
+      IMG_SRC_ELEMENT.src = "img/menu.svg";
+      body.style.overflow = "auto";
+    }
+};
+HAMBURGER_MENU_ELEMENT.addEventListener("click", hamburgerMenu);
+  
