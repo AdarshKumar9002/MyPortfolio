8d9a7670a3d32eb515aa0b04f0f3db36ac8f4931
AdarshKumar9002 <adarashkumar9002@gmail.com>
2024-01-17 13:38:44 +0530

    made the heroImage Rsponsive, fix the hover effect of navigation, added a see more button on project section.

--- src/script/script.js
+++ src/script/script.js
@@ -1,7 +1,11 @@
+// eslint-disable-next-line import/extensions
+import info from "./local.js";
+
 const HAMBURGER_MENU_ELEMENT =document.getElementById("btn-menu");
 const IMG_SRC_ELEMENT =document.getElementById("imgSrc");
 const NAV_CONTAINER_ELEMENT = document.querySelector(".nav__container");
-const body = document.querySelector("body");
+const BODY_ELEMENT = document.querySelector("body");
+// const HEROIMAGE_Text_ELEMENT = document.querySelector(".heroImage__text");
 
 const hamburgerMenu = () => {
     const visibility = NAV_CONTAINER_ELEMENT.getAttribute("data-visible");
@@ -9,13 +13,24 @@ const hamburgerMenu = () => {
       NAV_CONTAINER_ELEMENT.setAttribute("data-visible", true);
       HAMBURGER_MENU_ELEMENT.setAttribute("aria-expanded", true);
       IMG_SRC_ELEMENT.src = "img/x.svg";
-      body.style.overflow = "hidden"; 
+      BODY_ELEMENT.style.overflow = "hidden"; 
     } else {
       NAV_CONTAINER_ELEMENT.setAttribute("data-visible", false);
       HAMBURGER_MENU_ELEMENT.setAttribute("aria-expanded", false);
       IMG_SRC_ELEMENT.src = "img/menu.svg";
-      body.style.overflow = "auto";
+      BODY_ELEMENT.style.overflow = "auto";
     }
 };
 HAMBURGER_MENU_ELEMENT.addEventListener("click", hamburgerMenu);
   
+function heroInfo(name, degination, description) {
+  const potfolioers_name = document.getElementById("heroImage__text--name");
+  const potfolioers_degination = document.getElementById("heroImage__text--degination");
+  const potfolioers_description = document.getElementById("heroImage__text--intro");
+
+  potfolioers_name.innerHTML = name;
+  potfolioers_degination.innerHTML = degination;
+  potfolioers_description.innerHTML = description;
+}
+
+heroInfo(info.name, info.degination, info.description);
